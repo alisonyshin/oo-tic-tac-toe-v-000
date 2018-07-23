eeff6761e6c3e536e32024a8fe5d5cc149391aa0
Alison Shin <alisonyshin@gmail.com>
2018-07-23 03:04:04 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -62,12 +62,7 @@ class TicTacToe
   def won?(board)
     WIN_COMBINATIONS.detect do |win_combination|
     @board[win_combination[0]] == @board[win_combination[1]] && @board[win_combination[1]] == @board[win_combination[2]] && position_taken?(win_combination[0])
-      return win_combination
-    else
-      false
     end
-  end
-    return false
   end
 
 
