b8c5fa18b5cdf72f6fa4cde7beee10c717429aa1
Alison Shin <alisonyshin@gmail.com>
2018-07-23 02:59:46 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,6 +49,7 @@ class TicTacToe
     else
       turn
     end
+  end
     
     def current_player
       turn_count % 2 == 0 ? "X" : "O"
@@ -58,6 +59,23 @@ class TicTacToe
       @board.count{|token| token == "X" || token == "O"}
     end
     
+    def won?(board)
+      WIN_COMBINATIONS.each do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+      position_1 = board[win_index_1]
+      position_2 = board[win_index_2]
+      position_3 = board[win_index_3]
+      if position_1 == position_2 && position_2 == position_3 && position_taken?(board,win_index_1) == TRUE
+        return win_combination
+      else
+        false
+      end
+      #[position_1,position_2,position_3].all? {|position| position == "X"}
+      #[position_1,position_2,position_3].all? {|position| position == "O"}
+    end
+    return false
   end
 
 
